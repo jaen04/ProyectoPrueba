c5e1416a9af4af4b1adc502d4ae24d4887981192
jaen04 <hugo.perez.jaen@outlook.com>
2020-02-24 13:28:11 -0600

    first commit

---
+++ src/main/java/Prueba_1/prueba/App.java
@@ -0,0 +1,13 @@
+package Prueba_1.prueba;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Prueba_1/prueba/AppTest.java
@@ -0,0 +1,20 @@
+package Prueba_1.prueba;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
